44de056a7d37eec5d4939dd11e98b0371eabf155
arbab <arbab@aHorse.local>
2014-08-07 10:34:04 +0500

    Script for LAMP stack auto-installation

---
+++ LAMP.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#Instructions to use this script 
+#
+#chmod +x SCRIPTNAME.sh
+#
+#sudo ./SCRIPTNAME.sh
+
+
+echo "###################################################################################"
+echo "Please be Patient: Installation will start now.......and it will take some time :)"
+echo "###################################################################################"
+
+#Update the repositories
+
+sudo apt-get update
+
+#Apache, Php, MySQL and required packages installation
+
+sudo apt-get -y install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-mysql php5-gd php5-cli php5-dev mysql-client
+php5enmod mcrypt
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password MYPASSWORD'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password MYPASSWORD'
+sudo apt-get -y install mysql-server
+
+#Restart all the installed services to verify that everything is installed properly
+
+echo -e "\n"
+
+service apache2 restart && service mysql restart > /dev/null
+
+echo -e "\n"
+
+if [ $? -ne 0 ]; then
+   echo "Please Check the Install Services, There is some $(tput bold)$(tput setaf 1)Problem$(tput sgr0)"
+else
+   echo "Installed Services run $(tput bold)$(tput setaf 2)Sucessfully$(tput sgr0)"
+fi
+
+echo -e "\n"

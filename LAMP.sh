66f370823bb546c3db96a39907d0083fbbd7dbe4
arbabnazar <arbabnazar@users.noreply.github.com>
2014-08-07 10:42:42 +0500

    Update LAMP.sh

--- LAMP.sh
+++ LAMP.sh
@@ -20,8 +20,10 @@ sudo apt-get update
 sudo apt-get -y install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-mysql php5-gd php5-cli php5-dev mysql-client
 php5enmod mcrypt
 
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password MYPASSWORD'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password MYPASSWORD'
+#The following commands set the MySQL root password to MYPASSWORD123 when you install the mysql-server package.
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password MYPASSWORD123'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password MYPASSWORD123'
 sudo apt-get -y install mysql-server
 
 #Restart all the installed services to verify that everything is installed properly
